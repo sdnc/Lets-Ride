7f76715ca7df67178c89b2442bdffdb3994a3bf0
sdnc <dza.sara@gmail.com>
2022-01-13 13:59:55 +1100

    update vehicle speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
+    private float speed = 15.0f;
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
